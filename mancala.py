e6f7ee40b221acab132b46e19b1c1b116d488c24
RodrigoZea <zearodrigo37@gmail.com>
2020-10-12 17:55:42 -0600

    Agregando programa base de mancala

---
+++ mancala.py
@@ -0,0 +1,29 @@
+"""
+MODELACION Y SIMULACION
+MINI PROYECTO 4
+
+INTEGRANTES
+Sebastian Arriola
+Rodrigo Zea
+"""
+
+def main():
+    print("""
+    ¡Bienvenido al juego de Mancala
+    Los niveles de computadora disponibles son:
+    1. Noob
+    2. Avanzado
+    3. Pro
+    """)
+
+    cpu_lvl = input("Escoga el nivel de la computadora: ")
+
+def montecarlo(iters=0):
+    print("hola aca va montecarlo!!!")
+
+def game():
+    print("""
+    Imprimir interfaz de linea de comando jeje
+    """)
+
+main()
